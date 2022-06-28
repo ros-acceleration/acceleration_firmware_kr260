bfa6c06333a4f19ac8a849bf2999aa79b779e9d9
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2022-06-28 13:24:16 +0200

    Adjustments for enhancing sysroot with ROS 2
    Signed-off-by: Víctor Mayoral Vilches <v.mayoralv@gmail.com>

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,8 @@
 Changelog for package acceleration_firmware_kr260
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-1.0.0 (2022-06-27)
+v1.0.0 (2022-06-28)
 -------------------
+* Adjustments for enhancing sysroot with ROS 2
 * Create kr260 ROS mixins, leverage Ubuntu 22.04 sysroot
+* Initial commit
--- cmake/sysroot.cmake
+++ cmake/sysroot.cmake
@@ -5,8 +5,9 @@
 
 message(STATUS "Deploying sysroot")
 set(TARGET_SYSROOT_DIR ${FIRMWARE_DIR}/sysroots/aarch64-xilinx-linux)
+set(SYSROOTNAME "iot-limerick-kria-classic-desktop-2204-x06-20220614-78-sysroot.tar.xz")
 
 # extract sysroot and scripts
-run("${TESTFIRMWARE} tar -xf ${FIRMWARE_DIR}/iot-limerick-kria-classic-desktop-2204-x04-20220517-68-sysroot.tar.xz -C ${FIRMWARE_DIR}")
+run("${TESTFIRMWARE} tar -xf ${FIRMWARE_DIR}/${SYSROOTNAME} -C ${FIRMWARE_DIR}")
 
 message(STATUS "Ubuntu sysroot deployed.")
